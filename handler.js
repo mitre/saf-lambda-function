ca53674e682f6d80af87e33d3db12300838ee90f
Emily Rodriguez <ecrodriguez@mm279976-pc.lan>
2022-04-20 10:37:09 -0500

    delete workflows
    Signed-off-by: Emily Rodriguez <ecrodriguez@mm279976-pc.lan>

--- handler.js
+++ handler.js
@@ -21,44 +21,43 @@ async function getObject(bucket, objectKey) {
     }
 }
 
-module.exports.saf = async (event, context, callback) => {
-    const bucket = event.Records[0].s3.bucket.name;
-
-    const key = decodeURIComponent(
-        event.Records[0].s3.object.key.replace(/\+/g, " ")
-    );
-
-    console.log("Getting object with bucket: " + bucket + " and key: " + key);
-
-    const s3BucketObjectContents = await getObject(bucket, key);
-
-    // TODO: Explore the saf update to use stdin instead of a file for the contents
-    let HDF_FILE = path.resolve('/tmp/', key);
-    await fs.writeFileSync(HDF_FILE, s3BucketObjectContents);
-
-    const command_string_input = process.env.COMMAND_STRING_INPUT;
-    const command_string = `${command_string_input} -i ${HDF_FILE}`;
-
+async function runSaf(command_string) {
     if (!command_string) {
         throw new Error("SAF CLI Command String argument is required.");
     }
-
+    
     const saf_command = command_string.split(' ');
-
+    
     const allowable_topics = ['convert', 'generate', 'harden', 'scan', 'validate', 'view'];
     const topic = saf_command[0].split(':')[0];
-
+    
     if (!allowable_topics.includes(topic)) {
         throw new Error("The command string did not include one of the allowable topics: " + allowable_topics.join(', ') + ". Please reference the documentation for more details.");
     }
-
+    
     const command = saf_command[0].split(':')[1];
-
+    
     if (topic == "view" & command == "heimdall") {
         throw new Error("The SAF Action does not support the 'view heimdall' command. Please reference the documentation for other uses.");
     }
-
+    
     saf.run(saf_command);
+}
 
+module.exports.saf = async (event, context, callback) => {
+    const bucket = event.Records[0].s3.bucket.name;
+    const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
+    
+    console.log("Getting object with bucket: " + bucket + " and key: " + key);
+    const s3BucketObjectContents = await getObject(bucket, key);
+    
+    // TODO: Explore the saf update to use stdin instead of a file for the contents
+    let INPUT_FILE = path.resolve('/tmp/', key);
+    await fs.writeFileSync(INPUT_FILE, s3BucketObjectContents);
+    
+    const command_string_input = process.env.COMMAND_STRING_INPUT;
+    const command_string = `${command_string_input} -i ${INPUT_FILE}`;
+    
+    await runSaf(command_string);
     callback(null, `Completed saf function call with command ${command_string}`);
 };
